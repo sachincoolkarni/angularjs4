3058d11ed6619e6c7c42ee5ecd025c74845e156c
sachincoolkarni <sachincoolkarni@users.noreply.github.com>
2016-08-03 12:27:26 +0530

    Create a5.js

---
+++ a5.js
@@ -0,0 +1,12 @@
+var moduleAng=angular.module("myapp",[]);
+moduleAng.controller("mycontroller",function($scope)
+{
+	$scope.txt="HELLO";
+	
+
+});
+
+moduleAng.controller("mycontroller2",function($scope)
+{
+$scope.txt2="WELCOME TO ANGULARJS WORLD";
+});
